db68d06e75bb2a3afb114454e96f2c199d61b2bf
Nick <nicolas.cinera@gmail.com>
2022-06-05 20:38:53 -0500

    Update ServerApplication.java

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,5 +1,9 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.model.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +14,19 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	private CommandLineRunner run(ServerRepository serverRepository) {
+		return args ->
+			serverRepository.save(
+					Server
+							.builder()
+							.ipAddress("192.168.0.1")
+							.hostname("server1")
+							.memory("8GB")
+							.type("Arch Linux")
+							.status(Status.SERVER_UP)
+							.imageUrl("http://localhost:8080/server/images/server1.png")
+							.build()
+			);
+
+	}
 }
